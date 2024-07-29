0f5c1502e8cd423710c56869909eba4cf1d5a917
Chi_Tang <me@chitang.dev>
2024-07-29 13:13:31 +0800

    feat: added google ca and certificate chain check

--- main.py
+++ main.py
@@ -6,6 +6,64 @@ import telebot
 from os import getenv
 
 
+def verify_certificate_chain(keybox):
+	keybox = keybox.replace("\r\n", "\n")
+	keybox = keybox.split("</CertificateChain>")[0]
+	pattern = r"-----BEGIN CERTIFICATE-----\n.*?\n-----END CERTIFICATE-----"
+	certificates = re.findall(pattern, keybox, re.DOTALL)
+	if len(certificates) < 2:
+		return "❓ Invalid certificate chain"
+	elif len(certificates) == 2:
+		certificates = {"end_entity": certificates[0], "root": certificates[1]}
+	elif len(certificates) == 3:
+		certificates = {"end_entity": certificates[0], "intermediate": certificates[1], "root": certificates[2]}
+	else:
+		return "❓ Invalid certificate chain"
+	with tempfile.NamedTemporaryFile(delete=True) as root_cert_file:
+		root_cert_file.write(certificates['root'].encode())
+		root_cert_file.flush()
+		root_pubkey = subprocess.run(
+			['openssl', 'x509', '-in', root_cert_file.name, '-pubkey', '-noout'],
+			stdout=subprocess.PIPE,
+			stderr=subprocess.PIPE,
+			text=True
+		)
+		if root_pubkey.returncode != 0:
+			return f"OpenSSL error: {root_pubkey.stderr}"
+		if root_pubkey.returncode == 0:
+			with open("google_ca_pubkey.key", "r") as google_pubkey_file:
+				google_pubkey = google_pubkey_file.read()
+			if root_pubkey.stdout.encode() != google_pubkey.encode():
+				message = "❌ Root certificate is not signed by Google"
+			else:
+				message = "✅ Root certificate is signed by Google"
+		with tempfile.NamedTemporaryFile(delete=True) as end_entity_cert_file:
+			end_entity_cert_file.write(certificates['end_entity'].encode())
+			end_entity_cert_file.flush()
+			if "intermediate" in certificates:
+				with tempfile.NamedTemporaryFile(delete=True) as intermediate_cert_file:
+					intermediate_cert_file.write(certificates['intermediate'].encode())
+					intermediate_cert_file.flush()
+					result = subprocess.run(
+						['openssl', 'verify', '-CAfile', root_cert_file.name, '-untrusted', intermediate_cert_file.name, end_entity_cert_file.name],
+						stdout=subprocess.PIPE,
+						stderr=subprocess.PIPE,
+						text=True
+					)
+			else:
+				result = subprocess.run(
+					['openssl', 'verify', '-CAfile', root_cert_file.name, end_entity_cert_file.name],
+					stdout=subprocess.PIPE,
+					stderr=subprocess.PIPE,
+					text=True
+				)
+		if result.returncode != 0:
+			message += f"\n❌ Invalid certificate chain: {result.stderr}"
+		else:
+			message += "\n✅ Certificate chain is valid"
+		return message
+
+
 def extract_certificate_information(cert_pem):
 	with tempfile.NamedTemporaryFile(delete=True) as temp_cert_file:
 		temp_cert_file.write(cert_pem.encode())
@@ -24,13 +82,13 @@ def extract_certificate_information(cert_pem):
 	if match:
 		serial_number = hex(int(match.group(1).replace(":", ""), 16)).split("0x")[1]
 	else:
-		return "Cannot find serial number"
+		return "❌ Cannot find serial number"
 	pattern = r"Subject: "
 	match = re.search(pattern, cert_text, re.IGNORECASE)
 	if match:
 		subject = cert_text[match.end():].split("\n")[0]
 	else:
-		return "Cannot find subject"
+		return "❌ Cannot find subject"
 	return [serial_number, subject]
 
 
@@ -45,15 +103,16 @@ def common_handler(message):
 	file_info = bot.get_file(document.file_id)
 	file = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(API_TOKEN, file_info.file_path))
 	certificate = extract_certificate_information(file.text.split("<Certificate format=\"pem\">")[1].split("</Certificate>")[0])
-	reply = f"Serial Number: `{certificate[0]}`\nSubject: `{certificate[1]}`"
+	reply = f"ℹ️ Serial Number: `{certificate[0]}`\nℹ️ Subject: `{certificate[1]}`"
+	reply += f"\n{verify_certificate_chain(file.text)}"
 	try:
 		status = get_google_sn_list()['entries'][certificate[0]]
-		reply += f"\nSerial number found in Google's revoked keybox list\nReason: `{status['reason']}`"
+		reply += f"\n❌ Serial number found in Google's revoked keybox list\nReason: `{status['reason']}`"
 	except KeyError:
 		if certificate[0] == "4097":
-			reply += "\nAOSP keybox found, this keybox is untrusted"
+			reply += "\n❌ AOSP keybox found, this keybox is untrusted"
 		else:
-			reply += "\nSerial number not found in Google's revoked keybox list"
+			reply += "\n✅ Serial number not found in Google's revoked keybox list"
 	bot.reply_to(message, reply, parse_mode='Markdown')
 
 
